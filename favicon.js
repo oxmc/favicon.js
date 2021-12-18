fd9b86a7580162bb98d98ca5c5be4dbd3ab370e4
oxmc <67136658+oxmc@users.noreply.github.com>
2021-12-18 02:03:43 -0800

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -21,7 +21,10 @@ function testImage(url) {
 }
 
 async function favicon(icon, mode) {
-  var mode = mode || "png";
+  if (icon == null || icon == "") {
+	throw new Error('No icon provided!');
+  };
+  var mod = mode || "png";
   var favicon = document.querySelector("link[rel~='icon']");
   if (!favicon) {
     favicon = document.createElement('link');
@@ -30,36 +33,36 @@ async function favicon(icon, mode) {
     document.getElementsByTagName('head')[0].appendChild(favicon);
   };
   /*Check for valid image*/
-  if (mode == "base64") {
-    const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+  if (mod == "base64") {
+	const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
     if (base64regex.test(icon)) {
       /*Change icon now*/
       favicon.href = icon;
       if (favicon.href == icon) {
         console.log(`favicon.js: favicon Set successfully!`);
       } else {
-        console.log(`favicon.js: favicon Was not set!`);
+        throw new Error('Favicon was not set!');
       };
     };
   } else {
     var validimage = testImage(icon);
     validimage.then(
-     function(value) {
+      function(value) {
         if (value == "success") {
           /*Change icon now*/
           favicon.href = icon;
           if (favicon.href == icon) {
-            console.log(`favicon.js: favicon Set successfully!`);
+            console.log(`Image: ${icon}, Set successfully!`);
           } else {
-            console.log(`favicon.js: favicon Was not set!`);
+            throw new Error('Favicon was not set!');
           };
         } else if (value == "timeout") {
-          console.log(`Image: ${icon}, timedout, favicon was not set!`);
-        };
+		  throw new Error(`Image: ${icon}, timedout, favicon was not set!`);
+        }
       },
       function(error) {
-        console.log(`Image: ${icon}, is an invalid image, favicon was not set!`);
-      };
+		throw new Error(`Image: ${icon}, is an invalid image, favicon was not set!`);
+      }
     );
-  };
+  }
 };
